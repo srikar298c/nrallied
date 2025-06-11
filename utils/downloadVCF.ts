11f75fe72393d7e1b3c8c4d98dc44592dfe0156c
kudurmalla srikar <srikar2985969c@gmail.com>
2025-06-11 17:10:41 +0530

    refactor(downloadVCF): improve vcard generation and formatting
    - Split name into first/last components for proper N field formatting
    - Use array join with CRLF line breaks per VCF specification
    - Add URL protocol handling for website field
    - Improve code readability with consistent formatting

--- utils/downloadVCF.ts
+++ utils/downloadVCF.ts
@@ -1,16 +1,21 @@
-import { ContactInfo } from "@/types/contact"
+import { ContactInfo } from "@/types/contact";
 
 export const generateVCF = (contact: ContactInfo): string => {
-    return `BEGIN:VCARD
-  VERSION:3.0
-  FN:${contact.name}
-  N:${contact.name.split(' ').reverse().join(';')};;;
-  TEL;TYPE=CELL:${contact.phone}
-  ADR;TYPE=home:;;${contact.address.street};${contact.address.city};;${contact.address.zip};${contact.address.country}
-  EMAIL:${contact.email}
-  URL:https://${contact.website}
-  END:VCARD`
-  }
+  const [firstName = '', lastName = ''] = contact.name.split(' ');
+
+  return [
+    'BEGIN:VCARD',
+    'VERSION:3.0',
+    `FN:${contact.name}`,
+    `N:${lastName};${firstName};;;`,
+    `TEL;TYPE=CELL:${contact.phone}`,
+    `ADR;TYPE=home:;;${contact.address.street};${contact.address.city};;${contact.address.zip};${contact.address.country}`,
+    `EMAIL:${contact.email}`,
+    `URL:${contact.website.startsWith('http') ? contact.website : 'https://' + contact.website}`,
+    'END:VCARD'
+  ].join('\r\n'); // VCF spec recommends CRLF line breaks
+};
+
   
  export const downloadVCF = (contact: ContactInfo): void => {
     const vcfContent = generateVCF(contact)
